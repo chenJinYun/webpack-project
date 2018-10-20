73a79c23468cb92c2da40f06a36b0251bc4705fe
Kim Chen <KIM.CHEN@OOCL.COM>
2018-10-20 13:58:29 +0800

    add nav 逻辑处理

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,7 +1,36 @@
 /*
  * @Author: kim.chen 
- * @Date: 2018-10-18 18:28:44 
- * @Last Modified by:   kim.chen 
- * @Last Modified time: 2018-10-18 18:28:44 
+ * @Date: 2018-10-18 18:59:09 
+ * @Last Modified by: kim.chen
+ * @Last Modified time: 2018-10-20 13:54:49
  */
 require('./index.css');
+
+var _mm = require('util/mm.js');
+var _user = require('service/user-service.js');
+// 导航条
+var nav = {
+    init: function () {
+        this.bindEvent();
+    },
+    bindEvent: function () {
+        // 点击登录
+        $('.js-login').click(function () {
+            _mm.doLogin();
+        });
+        // 点击注册
+        $(".js-register").click(function (e) {
+            window.location.href = './user-register.html'
+        });
+        // 点击退出
+        $('.js-layout').click(function () {
+            _user.loyout(function(res){
+                window.location.reload();
+            },function(errMsg){
+                _mm.errorTips(errMsg);
+            })
+        })
+
+    }
+}
+nav.init();

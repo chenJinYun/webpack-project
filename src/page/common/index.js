0c35b29d3e544fb754ad5c2e13446d00847ea7f4
Kim Chen <KIM.CHEN@OOCL.COM>
2018-10-20 11:00:14 +0800

    common js require need file

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -1,7 +1,9 @@
 /*
  * @Author: kim.chen 
  * @Date: 2018-10-13 16:25:01 
- * @Last Modified by:   kim.chen 
- * @Last Modified time: 2018-10-13 16:25:01 
+ * @Last Modified by: kim.chen
+ * @Last Modified time: 2018-10-18 18:50:17
  */
-console.log('i am global');
+require('./layout.css');
+require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');

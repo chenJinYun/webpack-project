f5a26cab5fad878eab8c1e69ecc617eb174873e0
Kim Chen <KIM.CHEN@OOCL.COM>
2018-10-27 15:47:27 +0800

    finish add to cart

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -2,16 +2,22 @@
  * @Author: kim.chen 
  * @Date: 2018-10-18 18:59:09 
  * @Last Modified by: kim.chen
- * @Last Modified time: 2018-10-20 15:25:05
+ * @Last Modified time: 2018-10-27 13:06:01
  */
 require('./index.css');
 
 var _mm = require('util/mm.js');
 var _user = require('service/user-service.js');
+var _cart   = require('service/cart-service.js');
+
 // 导航条
 var nav = {
     init: function () {
         this.bindEvent();
+        this.loadUserInfo();
+        this.loadCartCount();
+        return this;
+
     },
     bindEvent: function () {
         // 点击登录
@@ -24,13 +30,30 @@ var nav = {
         });
         // 点击退出
         $('.js-layout').click(function () {
-            _user.loyout(function(res){
+            _user.loyout(function (res) {
                 window.location.reload();
-            },function(errMsg){
+            }, function (errMsg) {
                 _mm.errorTips(errMsg);
             })
         })
 
+    },
+    // 加载用户信息
+    loadUserInfo: function () {
+        _user.checkLogin(function (res) {
+            $('.user.not-login').hide().siblings('.user.login').show()
+                .find('.username').text(res.username);
+        }, function (errMsg) {
+            // do nothing
+        });
+    },
+    // 加载购物车数量
+    loadCartCount: function () {
+        _cart.getCartCount(function (res) {
+            $('.nav .cart-count').text(res || 0);
+        }, function (errMsg) {
+            $('.nav .cart-count').text(0);
+        });
     }
 }
 module.exports = nav.init();
--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -2,7 +2,7 @@
  * @Author: kim.chen 
  * @Date: 2018-10-27 10:18:42 
  * @Last Modified by: kim.chen
- * @Last Modified time: 2018-10-27 11:19:26
+ * @Last Modified time: 2018-10-27 12:09:30
  */
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
@@ -29,12 +29,42 @@ var page = {
     },
     bindEvent: function () {
         var _this = this;
+        // 图片预览
+        $(document).on('mouserenter', '.p-img-item', function () {
+            let imageUrl = $(this).find('.p-img').attr(src);
+            $('.main-img').attr('src', imageUrl);
+        })
+        // count操作
+        $(document).on('click', '.p-count-btn', function () {
+            var type = $(this).hasClass('plus') ? 'plus' : 'minus',
+                $pCount = $('.p-count'),
+                currCount = parseInt($pCount.val()),
+                minCount = 1,
+                maxCount = _this.data.detailInfo.stocl || 1;
+            if (type === 'plus') {
+                $pCount.val(currCount < maxCount ? currCount + 1 : maxCount)
+            } else if (type = "minus") {
+                $pCount.val(currCount > minCount ? currCount - 1 : minCount)
+
+            }
+        });
+        // 加入购物车
+        $(document).on('click', '.cart-add', function () {
+            _cart.addToCart({
+                productId: _this.data.productId,
+                count: $('.p-count').val()
+            }, function (res) {
+                window.location.href = './result.html?type=cart-add';
+            }, function (err) {
+                _mm.errorTips(err)
+            })
+        })
     },
     // 加载商品详情
     loadDetail: function () {
-        let _this=this;
+        let _this = this;
         let html = '';
-        let $pageWrap=$('.page-wrap')
+        let $pageWrap = $('.page-wrap')
         // loading
         $pageWrap.html("<div class='loading'></div>")
         // 请求详细信息
@@ -47,8 +77,8 @@ var page = {
         })
     },
     // 数据匹配
-    filter:function(data){
-        data.subImages=data.subImages.split(',')
+    filter: function (data) {
+        data.subImages = data.subImages.split(',')
     }
 }
 $(function () {

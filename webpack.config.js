1e079d50d4fdf7c61ce7d9ec478943030859f7f3
Kim Chen <KIM.CHEN@OOCL.COM>
2018-10-27 15:46:50 +0800

    finish  cart

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: kim.chen 
  * @Date: 2018-10-13 15:21:56 
  * @Last Modified by: kim.chen
- * @Last Modified time: 2018-10-27 10:17:25
+ * @Last Modified time: 2018-10-27 14:02:09
  */
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -26,6 +26,7 @@ var config = {
         'index' : ['./src/page/index/index.js'],
         'list' : ['./src/page/list/index.js'],
         'detail' : ['./src/page/detail/index.js'],
+        'cart' : ['./src/page/cart/index.js'],
         'user-login' : ['./src/page/user-login/index.js'],
         'user-register' : ['./src/page/user-register/index.js'],
         'user-pass-reset' : ['./src/page/user-pass-reset/index.js'],
@@ -36,7 +37,7 @@ var config = {
     },
     output: { //出口文件
         path: "./dist",
-        publicPath: '/dist',
+        publicPath: '/dist/',
         filename: 'js/[name].js'
     },
     externals: { //全局加载jquery
@@ -77,6 +78,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
         new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
         new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
+        new HtmlWebpackPlugin(getHtmlConfig('cart', '商品详情页')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login','用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register','用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset','找回密码')),
